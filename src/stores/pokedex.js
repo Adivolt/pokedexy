3b51898115f9c86280054edd4b5447abb0e4c579
miomech <29011382+miomech@users.noreply.github.com>
2023-05-07 13:31:06 -0500

    refactored pokedex stores

--- src/stores/pokedex.js
+++ src/stores/pokedex.js
@@ -1,112 +1,37 @@
 import { defineStore } from "pinia";
 
 // index 0 is national dex
-const DEFAULT_DEX_NATIONAL = 0;
-// index 24 is pikachu
-const DEFAULT_POKEMON_PIKACHU = 24;
+const KANTO_POKEDEX = 1;
 
 export const usePokedexStore = defineStore("pokedex", {
+
     state: () => ({
-        available_regions: "",
-        selected_region: "",
-        searchable_pokemon: "",
-        selected_pokemon: "",
-        pokemon_data: "",
+        regions: null,
+        active_region: null
     }),
 
-    getters: {
-        formatted_regions: (state) =>
-            state.available_regions.map((region) => ({
-                name: region.name.replace("-", " "),
-                url: region.url,
-            })),
-    },
-
     actions: {
-        // Fetch a list of all available Pokédex regions
-        fill() {
-            fetch("https://pokeapi.co/api/v2/pokedex/?limit=20")
+        async init() {
+            await fetch("https://pokeapi.co/api/v2/pokedex/?limit=20")
                 .then((response) => response.json())
                 .then((data) => data.results)
-                .then(async (regions) => {
-                    // Set the regions available for selection
-                    this.available_regions = regions;
-                    // Set the default region "0" is the national dex
-                    this.selected_region = regions[DEFAULT_DEX_NATIONAL];
-
-                    // fill the pokedex with the selected region
-                    let fetched_pokemon_from_region =
-                        await this.fill_pokedex_pokemon(this.selected_region);
-
-                    // Set the searchable Pokémon once the region pokemon are retrieved
-                    this.searchable_pokemon = await fetched_pokemon_from_region;
-                    // Set the default Pokémon to pikachu on load index is "25"
-                    await this.set_pokemon(
-                        await fetched_pokemon_from_region[
-                            DEFAULT_POKEMON_PIKACHU
-                        ]
-                    );
-                });
+                .then((regions) => {
+                    this.regions = format_region_data(regions);
+                })
+                .finally(() => this.set_region(this.regions[KANTO_POKEDEX]))
+            ;
         },
 
-        // Fetch the Pokémon available in this region
-        async fill_pokedex_pokemon(region) {
-            return await fetch(region.url)
-                .then((response) => response.json())
-                .then((data) => data.pokemon_entries)
-                .then((entries) => {
-                    return generate_species_id(entries);
-                });
-        },
+        set_region(region) {
+            this.active_region = region;
+        }
+    }
 
-        // Set the Pokédex region
-        async set_region(region) {
-            this.selected_region = region;
-            this.searchable_pokemon = await this.fill_pokedex_pokemon(region);
-        },
-
-        // Set the Pokémon from region dex
-        async set_pokemon(pokemon) {
-            this.selected_pokemon = pokemon;
-            let [pokemon_info, pokemon_species_info] = await Promise.all([
-                fetch(
-                    `https://pokeapi.co/api/v2/pokemon/${pokemon.species_id}/`
-                )
-                    .then((response) => response.json())
-                    .then((data) => (this.selected_pokemon_information = data)),
-                fetch(
-                    `https://pokeapi.co/api/v2/pokemon-species/${pokemon.species_id}/`
-                )
-                    .then((response) => response.json())
-                    .then(
-                        (data) =>
-                            (this.selected_pokemon_species_information = data)
-                    ),
-            ]);
-
-            // Merge the data from the 2 api calls and create a new object based on the data
-            this.pokemon_data = {
-                ...pokemon_info,
-                ...pokemon_species_info,
-            };
-        },
-    },
 });
 
-// * The point of this optimization is to reduce requests to the poke api,
-// we can extract the id from the Pokémon species url and attach it as a property
-// this id is also used to accurately set the Pokémon icon on the search bar
-function generate_species_id(entries) {
-    return entries.map((pokemon) => {
-        // Remove the trailing slash from the url
-        let cleaned_url = pokemon.pokemon_species.url.slice(0, -1);
-        // Get the id from the last segment of the url
-        let detected_species_id = cleaned_url.split("/").pop();
-
-        return {
-            entry_number: pokemon.entry_number,
-            species_id: detected_species_id,
-            pokemon_species: pokemon.pokemon_species,
-        };
-    });
+function format_region_data(regions) {
+    return regions.map((region) => ({
+        name: region.name.replace("-", " "),
+        url: region.url
+    }));
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,30 +12,30 @@ const pokedexStore = usePokedexStore();
     <section>
         <div class="bg-gray-700 flex p-1 w-full gap-1 sticky top-[5vh]">
             <TheRegionSelector />
-            <ThePokemonSearchField />
+            <!--<ThePokemonSearchField />-->
         </div>
 
-        <p class="font-bungee text-sm py-1 px-2 text-slate-500">
-            {{ pokedexStore.searchable_pokemon.length }} Pokémon in {{ pokedexStore.selected_region.name }}
-        </p>
+        <!--<p class="font-bungee text-sm py-1 px-2 text-slate-500">-->
+        <!--    {{ pokedexStore.searchable_pokemon.length }} Pokémon in {{ pokedexStore.selected_region.name }}-->
+        <!--</p>-->
 
-        <div class="grid grid-cols-3 gap-2 py-1 px-2">
-            <div
-                v-for="pokemon in pokedexStore.searchable_pokemon"
-                :key="pokemon.species_id"
-                class="border-[1px] rounded-md border-slate-600"
-            >
-                <a :href="`/${pokemon.species_id}/basic/`">
-                    <v-lazy-image
-                        :src=" pokemon?.species_id ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.species_id}.png` : './img/pokeball-grayscale.png'"
-                        src-placeholder="/img/loading-spinner.gif"
-                        class="w-full h-auto p-2"
-                    />
-                    <p class="text-center font-bungee text-xs p-1 text-slate-600">
-                        {{ pokemon.pokemon_species.name }}
-                    </p>
-                </a>
-            </div>
-        </div>
+        <!--<div class="grid grid-cols-3 gap-2 py-1 px-2">-->
+        <!--    <div-->
+        <!--        v-for="pokemon in pokedexStore.searchable_pokemon"-->
+        <!--        :key="pokemon.species_id"-->
+        <!--        class="border-[1px] rounded-md border-slate-600"-->
+        <!--    >-->
+        <!--        <a :href="`/${pokemon.species_id}/basic/`">-->
+        <!--            <v-lazy-image-->
+        <!--                :src=" pokemon?.species_id ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.species_id}.png` : './img/pokeball-grayscale.png'"-->
+        <!--                src-placeholder="/img/loading-spinner.gif"-->
+        <!--                class="w-full h-auto p-2"-->
+        <!--            />-->
+        <!--            <p class="text-center font-bungee text-xs p-1 text-slate-600">-->
+        <!--                {{ pokemon.pokemon_species.name }}-->
+        <!--            </p>-->
+        <!--        </a>-->
+        <!--    </div>-->
+        <!--</div>-->
     </section>
 </template>
